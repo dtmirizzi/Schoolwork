183f10529e86aeb429c247fb7ad32793dea59bdc
dtmirizzi <dtmirizzi@gmail.com>
2015-12-10 19:22:32 -0800

    add henry print

--- Java-CSC-210/Assignment 2/HW2_4.java
+++ Java-CSC-210/Assignment 2/HW2_4.java
@@ -7,6 +7,7 @@ public class HW2_4 {
         int input = 10;
         if (input % 5 == 0) {
             System.out.println(input + " " + "Is Divisible By 5");
+	    system.out.println("hi Henry");
         } else {
             System.out.println(input + " " + "Isn't Divisible By 5");
         }
